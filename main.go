2602924a1e4dd91dd19692ca16feba01d6371140
whilesun <1207460050@qq.com>
2023-03-06 22:20:55 +0800

    main init

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
+)
+
+func main() {
+	handler := gin.New()
+	handler.GET("/test", func(context *gin.Context) {
+		context.String(http.StatusOK, "hello gin delete method")
+	})
+	s := &http.Server{
+		Addr:         ":8089",
+		Handler:      handler,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+	s.ListenAndServe()
+}
